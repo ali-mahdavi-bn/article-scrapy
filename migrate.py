419da4b3734d8037ee6cd976ed86649dd89d2cfa
ali mahdavi <ali@alis-MacBook-Air.local>
2023-12-25 16:19:20 +0330

    clear file in root

--- migrate.py
+++
@@ -1,7 +0,0 @@
-from backbone.infrastructure.postgres_connection import BaseEntity, DEFAULT_ENGIN
-from migrate_enum import migrate_enumerations
-
-migrate_enumerations()
-BaseEntity.metadata.create_all(DEFAULT_ENGIN)
-
-
